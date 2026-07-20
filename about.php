e623c10d8824e6602032c433bed80c7116591c7d
OatDe v <surapich56@gmail.com>
2026-07-21 00:15:16 +0700

    Change to html

--- about.php
+++
@@ -1,8 +0,0 @@
-<section class="about fade-section" id="about">
-    <div class="container">
-        <h2 class="section-title">About Me 📚</h2>
-        <p class="section-text">
-        Hi everyone, my name is Surapich Klaeothanong. I am currently studying at Mahasarakham University, majoring in Computer Science. At present, I have skills in coding and web development, and I am continuously working to improve myself every day. I am ready to apply my skills to my future career. <br><br><br> Let me share more about myself.
-        </p>
-    </div>
-</section>
